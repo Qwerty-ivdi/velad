c26c3352f58a18a234ac3ad02220dc1717bf51a6
Qwerty-ivdi <dimaivanov64101@gmail.com>
2026-06-03 00:45:05 +0300

    twitch stat1

--- backend/run.py
+++ backend/run.py
@@ -5,5 +5,5 @@ import os
 app, socketio = create_app()
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    socketio.run(app, host='0.0.0.0', port=port, debug=True)
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host='0.0.0.0', port=port)
