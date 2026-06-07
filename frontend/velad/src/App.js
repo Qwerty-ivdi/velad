5281802ddd9a8c9d3089e569a4f56b0accbc928e
Qwerty-ivdi <dimaivanov64101@gmail.com>
2026-06-08 02:08:50 +0300

    fix import

--- frontend/velad/src/App.js
+++ frontend/velad/src/App.js
@@ -15,6 +15,7 @@ import SearchPage from './components/search/SearchPage';
 import './styles/global.css';
 import './styles/navbar.css';
 import './styles/ProfileHeader.css';
+import { useSocket } from './hooks/useSocket';
 
 
 function App() {
