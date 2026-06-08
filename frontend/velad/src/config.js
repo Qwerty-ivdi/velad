f05506228d15a3be151f44973249ffcd4a00ef1d
Qwerty-ivdi <dimaivanov64101@gmail.com>
2026-06-08 11:05:00 +0300

    123rehfesf

--- frontend/velad/src/config.js
+++ frontend/velad/src/config.js
@@ -1,3 +1,3 @@
 // src/config.js
-export const API_URL = 'https://velad.onrender.com/api';
-export const SOCKET = 'https://velad.onrender.com';
+export const API_URL = 'https://velad-production.up.railway.app/api';
+export const SOCKET = 'https://velad-production-c7d5.up.railway.app';
